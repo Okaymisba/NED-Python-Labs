9bd7f8248107ce01516d3184e7f64808c9da7ed5
okaymisba <msbahsarfaraz@gmail.com>
2024-08-27 20:44:53 +0500

    Added a Program to Calculate the volume of the sphere

---
+++ Lab 1 (27-August-2024)/volumeOfSphere.py
@@ -0,0 +1,5 @@
+from math import *
+
+radius = int(input("Enter The Radius of Sphere in Centimeters: "))
+volume = 4 / 3 * pi * radius ** 3
+print(f"The Volume of the Sphere of Radius {radius} is {volume}")
