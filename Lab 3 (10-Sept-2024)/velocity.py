84243c71c5d72de45e1dfc09dcbfb1fbcfde09e5
Okaymisba <msbahsarfaraz@gmail.com>
2024-09-15 20:50:59 +0500

    Add files via upload
    Added Lab 3 (10-Sept-2024)

---
+++ Lab 3 (10-Sept-2024)/velocity.py
@@ -0,0 +1,4 @@
+angularVelocity = 10
+radius = float(input("Enter the Distance in meters from the centre: "))
+linearVelocity = radius * angularVelocity
+print(f"The Linear Velocity of the Ball is {linearVelocity}")
