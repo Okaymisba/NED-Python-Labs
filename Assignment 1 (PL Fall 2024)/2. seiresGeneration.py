6412c03b5f18c17dd812bf75230c1aab39a0ded1
Okaymisba <msbahsarfaraz@gmail.com>
2024-09-24 20:55:28 +0500

    Update 2. seiresGeneration.py

--- Assignment 1 (PL Fall 2024)/2. seiresGeneration.py
+++ Assignment 1 (PL Fall 2024)/2. seiresGeneration.py
@@ -1,3 +1,17 @@
-for num in range(2,9):
-    series = (num * 5) + 1
-    print(series, end=" ")
+# For First Series
+for num in range(11, 42, 5):
+    print(num, end=" ")
+print()
+
+# For Second Series
+for num in range(3, 34, 5):
+    print(num, end=" ")
+print()
+
+# For Third Series
+for num in range(20, 10, -3):
+    print(num, end=" ")
+    if num == 11:
+        break
+    print(num, end=" ")
+print()
