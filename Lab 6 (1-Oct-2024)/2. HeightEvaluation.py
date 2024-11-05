a40865548f3ee4b5364f8bf224eeea8fd151f1f5
Okaymisba <msbahsarfaraz@gmail.com>
2024-11-05 10:00:43 +0500

    Formatted 2. HeightEvaluation.py

--- Lab 6 (1-Oct-2024)/2. HeightEvaluation.py
+++ Lab 6 (1-Oct-2024)/2. HeightEvaluation.py
@@ -1,9 +1,11 @@
 import math
 
+
 def ladder_height(length, angle_degrees):
     angle_radians = math.pi * angle_degrees / 180
     height = length * math.sin(angle_radians)
     return height
+
 
 print("Height for (a) 16 feet and 75 degrees:", ladder_height(16, 75))
 print("Height for (b) 20 feet and 0 degrees:", ladder_height(20, 0))
