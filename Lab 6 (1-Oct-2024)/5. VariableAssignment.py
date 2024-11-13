170f667369a795b95dde7dfd59926101e85a7433
Okaymisba <msbahsarfaraz@gmail.com>
2024-11-13 20:34:14 +0500

    Added Remaining files for lab 6, now only 3. Expressions is left

---
+++ Lab 6 (1-Oct-2024)/5. VariableAssignment.py
@@ -0,0 +1,17 @@
+# (a)Assign 6 to variable a and 7 to variable b.
+a = 6
+b = 7
+
+# (b)Assign to variable c the average of variables a and b.
+c = (a + b) / 2
+
+# (c)Assign to variable inventory the list containing strings 'paper', 'staples', and 'pencils'.
+inventory = ['paper', 'staples', 'pencils']
+
+# (d)Assign to variables first, middle and last the strings 'John', 'Fitzgerald', and 'Kennedy'.
+first = 'John'
+middle = 'Fitzgerald'
+last = 'Kennedy'
+
+# (e)Assign to variable fullname the concatenation of string variables first, middle, and last.
+fullname = first + ' ' + middle + ' ' + last
