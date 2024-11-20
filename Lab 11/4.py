a0890fecbdccbccec62eaa6f5ee596a60d4bc870
Okaymisba <msbahsarfaraz@gmail.com>
2024-11-20 18:44:57 +0500

    Update 4.py

--- Lab 11/4.py
+++ Lab 11/4.py
@@ -20,7 +20,7 @@ dishes_cooked = {
 
 cooked_favorites = [dish for dish in favorite_dishes if dish in dishes_cooked]
 
-print(f"You will get {len(cooked_favorites)} of your favorite dishes cooked this week.\n")
+print(f"You will get {len(cooked_favorites)} favorite dishes cooked this week.\n")
 print("The favorite dishes cooked this week are:")
 for dish in cooked_favorites:
     print(f"- {dish}")
