e2df127d3ad0e38160fcefa88c8a95221545445e
Okaymisba <msbahsarfaraz@gmail.com>
2024-11-20 18:56:08 +0500

    Update 1.py

--- Lab 12/1.py
+++ Lab 12/1.py
@@ -1,8 +1,6 @@
 import random
 
-cities = ["Karachi", "Lahore", "Islamabad", "Rawalpindi", "Peshawar",
-          "Quetta", "Multan", "Faisalabad", "Sialkot", "Gujranwala",
-          "Sukkur", "Murree", "Bahawalpur", "Hyderabad", "Gawadar"]
+cities = ["Sukkur", "Murree", "Bahawalpur", "Hyderabad", "Gawadar"]
 
 random.shuffle(cities)
 
